253558309dd12195174437bf6959a894787da8d4
Koen Volkers <k.volkers@triple-it.nl>
2015-12-08 19:28:10 +0100

    Initial commit

---
+++ src/main/java/nl/riebie/mcclans/MCClans.java
@@ -0,0 +1,10 @@
+package nl.riebie.mcclans;
+
+import org.spongepowered.api.plugin.Plugin;
+
+/**
+ * Created by K.Volkers on 8-12-2015.
+ */
+@Plugin(id = "nl.riebie.MCClans", name = "MCClans", version = "1.0")
+public class MCClans {
+}
