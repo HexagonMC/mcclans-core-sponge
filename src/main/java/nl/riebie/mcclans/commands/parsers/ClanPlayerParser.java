c00a41b063fe8fe94aef439564ff2392cb0983c5
Koen Volkers <volkers.k@gmail.com>
2016-06-11 21:49:45 +0200

    Added temporary manual selector parsing for @p until https://github.com/SpongePowered/SpongeCommon/issues/791 is fixed.

--- src/main/java/nl/riebie/mcclans/commands/parsers/ClanPlayerParser.java
+++ src/main/java/nl/riebie/mcclans/commands/parsers/ClanPlayerParser.java
@@ -22,6 +22,8 @@
 
 package nl.riebie.mcclans.commands.parsers;
 
+import com.flowpowered.math.vector.Vector3d;
+import com.google.common.collect.Lists;
 import nl.riebie.mcclans.ClansImpl;
 import nl.riebie.mcclans.api.ClanPlayer;
 import nl.riebie.mcclans.commands.filledparameters.NormalFilledParameter;
@@ -30,14 +32,12 @@ import nl.riebie.mcclans.player.ClanPlayerImpl;
 import nl.riebie.mcclans.utils.UUIDUtils;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.source.LocatedSource;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.selector.Selector;
 
-
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Created by riebie on 12/03/2016.
@@ -46,30 +46,55 @@ public class ClanPlayerParser implements ParameterParser<ClanPlayer> {
     @Override
     public ParseResult<ClanPlayer> parseValue(CommandSource commandSource, String value, NormalFilledParameter parameter) {
         ClanPlayerImpl clanPlayer = null;
-        if(value.startsWith("@")){
-           Set<Entity> entities = Selector.parse(value).resolve(commandSource);
-            if(entities.size() > 0 && entities.toArray()[0] instanceof Player){
-                UUID uuid = ((Player)entities.toArray()[0]).getUniqueId();
+        if (value.startsWith("@p")) {
+            clanPlayer = tempParseNearestPlayer(commandSource);
+            if (clanPlayer == null) {
+                return ParseResult.newErrorResult("Selector produced no results");
+            }
+        } else if (value.startsWith("@")) {
+            Set<Entity> entities = Selector.parse(value).resolve(commandSource);
+            if (entities.size() > 0 && entities.toArray()[0] instanceof Player) {
+                UUID uuid = ((Player) entities.toArray()[0]).getUniqueId();
                 clanPlayer = ClansImpl.getInstance().getClanPlayer(uuid);
-            } else{
-                return ParseResult.newErrorResult("Problem with selector");
+            } else {
+                return ParseResult.newErrorResult("Selector produced no results");
             }
 
-        } else{
+        } else {
             clanPlayer = ClansImpl.getInstance().getClanPlayer(value);
         }
 
 
         if (clanPlayer != null) {
-            return ParseResult.newSuccessResult((ClanPlayer)clanPlayer);
+            return ParseResult.newSuccessResult((ClanPlayer) clanPlayer);
         } else {
             UUID playerUUID = UUIDUtils.getUUID(value);
             Optional<Player> playerOp = playerUUID == null ? Optional.empty() : Sponge.getServer().getPlayer(playerUUID);
-            if(playerOp.isPresent()){
+            if (playerOp.isPresent()) {
                 return ParseResult.newSuccessResult(ClansImpl.getInstance().createClanPlayer(playerUUID, value));
             } else {
                 return ParseResult.newErrorResult(Messages.PLAYER_DOES_NOT_EXIST);
             }
+        }
+    }
+
+    /**
+     * TODO remove once https://github.com/SpongePowered/SpongeCommon/issues/791 is fixed
+     */
+    private ClanPlayerImpl tempParseNearestPlayer(CommandSource commandSource) {
+        if (!(commandSource instanceof LocatedSource)) {
+            return null;
+        }
+
+        Vector3d commandSourcePosition = ((LocatedSource) commandSource).getLocation().getPosition();
+        Collection<Player> onlinePlayers = Sponge.getServer().getOnlinePlayers();
+        List<Player> sortedPlayers = Lists.newArrayList(onlinePlayers);
+        Collections.sort(sortedPlayers, Comparator.comparing(player -> commandSourcePosition.sub(player.getLocation().getPosition()).abs()));
+        if (!onlinePlayers.isEmpty()) {
+            UUID uuid = sortedPlayers.get(0).getUniqueId();
+            return ClansImpl.getInstance().getClanPlayer(uuid);
+        } else {
+            return null;
         }
     }
 }
